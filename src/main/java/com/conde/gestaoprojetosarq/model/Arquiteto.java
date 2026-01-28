3e0d8309017e86732121c428eb5442725fdd4783
Luiz Conde <luizconde.deev@gmail.com>
2026-01-27 22:30:01 -0300

    Criando Entity para Arquitetos

---
+++ src/main/java/com/conde/gestaoprojetosarq/model/Arquiteto.java
@@ -0,0 +1,33 @@
+package com.conde.gestaoprojetosarq.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "arquitetos")
+public class Arquiteto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "cpf", length = 14)
+    private String cpf;
+    @Column(name = "cau", length = 20)
+    private String cau;
+
+    @OneToMany(mappedBy = "arquiteto", cascade = CascadeType.ALL)
+    List<Projeto> projetos;
+}
