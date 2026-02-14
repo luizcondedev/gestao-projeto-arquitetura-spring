fa2d2ac49a0026f24bf832156abe42e4a6c59e59
Luiz Conde <luizconde.deev@gmail.com>
2026-02-14 17:18:54 -0300

    feat: create List methods

--- src/main/java/com/conde/gestaoprojetosarq/model/converters/ClienteConverter.java
+++ src/main/java/com/conde/gestaoprojetosarq/model/converters/ClienteConverter.java
@@ -1,12 +1,16 @@
 package com.conde.gestaoprojetosarq.model.converters;
 
 import com.conde.gestaoprojetosarq.model.Cliente;
+import com.conde.gestaoprojetosarq.model.Projeto;
 import com.conde.gestaoprojetosarq.model.dto.ClienteDTO;
+import com.conde.gestaoprojetosarq.model.dto.ProjetoDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ClienteConverter {
-    public ClienteDTO paraDTO(Cliente cliente){
+    public ClienteDTO paraDTO(Cliente cliente) {
         return ClienteDTO.builder()
                 .nome(cliente.getNome())
                 .email(cliente.getEmail())
@@ -15,5 +19,23 @@ public class ClienteConverter {
                 .build();
     }
 
+    public List<ClienteDTO> paraListaClienteDTO(List<Cliente> ClienteList){
+        return ClienteList.stream().map(this::paraDTO).toList();
+    }
+
+    public List<ProjetoDTO> paraListaProjetoDTO(List<Projeto> ProjetoList){
+        return ProjetoList.stream().map(this::paraProjetoDTO).toList();
+    }
+
+    public ProjetoDTO paraProjetoDTO(Projeto projeto){
+        return ProjetoDTO.builder()
+                .nomeProjeto(projeto.getNomeProjeto())
+                .enderecoProjeto(projeto.getEnderecoProjeto())
+                .faseProjeto(projeto.getFaseProjeto())
+                .orcamento(projeto.getOrcamento())
+                .nomeArquiteto(projeto.getArquiteto().getNome())
+                .nomeCliente(projeto.getCliente().getNome())
+                .build();
+    }
 
 }
